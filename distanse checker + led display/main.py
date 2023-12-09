db70f1eb948106362d424ccc6efaf3c3b7949d37
Patryk360 <nexigamingyt@gmail.com>
2023-12-09 19:42:42 +0100

    new functions

--- distanse checker + led display/main.py
+++ distanse checker + led display/main.py
@@ -2,7 +2,11 @@ import sys
 sys.path.append("/libs")
 from machine import Pin, I2C
 from pico_i2c_lcd import I2cLcd
+from imu import MPU6050
 from utime import sleep, sleep_us, ticks_us
+
+i2c_mtu = I2C(1, sda=Pin(2), scl=Pin(3), freq=400000)
+imu = MPU6050(i2c_mtu)
 
 i2c = I2C(0, sda=Pin(12), scl=Pin(13), freq=400000)
 lcd = I2cLcd(i2c, 0x27, 2, 16)
@@ -11,7 +15,7 @@ trig = Pin(15, Pin.OUT)
 echo = Pin(14, Pin.IN)
 led = Pin(25, Pin.OUT)
 
-fps = 10
+fps = 1
 
 def distanse():
     trig.value(0)
@@ -30,8 +34,13 @@ def distanse():
     distanse = (((timeresult * (10 ** -6)) * 343.8 / 2)) * 100
     
     my_distanse = str(round(distanse, 2))
+    tem=str(round(imu.temperature,2))
+    x=str(round(imu.gyro.x))
+    y=str(round(imu.gyro.y))
+    z=str(round(imu.gyro.z))
+    print(" "+x+" "+y+" "+z)
     lcd.clear()
-    lcd.putstr("Dystans: \n"+my_distanse+" cm")
+    lcd.putstr("D: "+my_distanse+" cm\nT: "+ tem + " C")
     sleep(1/fps)
 
 while True:
