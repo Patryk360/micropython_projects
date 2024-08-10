389840a919cd3c4524961bea46b8b3ec95d528de
Patryk360 <nexigamingyt@gmail.com>
2024-08-10 21:16:50 +0200

    fix

--- paper rc boat/boat/controlers/servoControler.py
+++ paper rc boat/boat/controlers/servoControler.py
@@ -1,9 +1,21 @@
 from machine import Pin, PWM
 setDegree = 0
 
+servo = PWM(Pin(9))
+servo.freq(50)
+
 def setServoDegree(degree):
     global setDegree
     setDegree = degree
+    
+    servoMinPWM = 3277 #1802
+    servoMaxPWM = 6554 #7864
+    
+    duty = int((degree / 180) * (servoMaxPWM - servoMinPWM) + servoMinPWM)
+
+    print(duty)
+    
+    servo.duty_u16(duty)
 
 def getServoDegree():
     return setDegree
--- paper rc boat/boat/main.py
+++ paper rc boat/boat/main.py
@@ -8,6 +8,12 @@ from imuControler import temperature, gyro
 from servoControler import setServoDegree, getServoDegree
 from wifiControler import connectToAP, connectToBoat, send
 
+setServoDegree(15)
+sleep(1)
+setServoDegree(90)
+sleep(1)
+print(str(getServoDegree()))
+
 connectToAP("PILOT_RC", "T7es7yeumU[WQFv#tWub")
 connectToBoat("192.168.4.1", 3333)
 
@@ -17,10 +23,6 @@ sleep(2)
 motorRight()
 sleep(2)
 motorStop()
-
-setServoDegree(11)
-sleep(1)
-print(str(getServoDegree()))
 
 while True:
     temp = temperature()
