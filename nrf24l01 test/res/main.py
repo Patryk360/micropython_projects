eb240be57fad8a6ffceecc37570714bc0c80b0a1
Patryk360 <nexigamingyt@gmail.com>
2024-09-02 09:03:38 +0200

    update

--- nrf24l01 test/res/main.py
+++ nrf24l01 test/res/main.py
@@ -1,7 +1,21 @@
 import sys
 sys.path.append("/libs")
-from machine import Pin
+from machine import Pin, SoftI2C
 from time import sleep
 import nrf24l01test
+from ssd1306 import SSD1306_I2C
 
-nrf24l01test.responder()
+i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
+
+oled.text('Hello, World!', 0, 0)
+oled.show()
+
+sleep(3)
+oled.fill(0)
+oled.text('On!', 0, 0)
+oled.show()
+
+nrf24l01test.initiator()
