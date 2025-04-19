484c48560ffcfcdaad3148be736adad960983b5e
Patryk360 <nexigamingyt@gmail.com>
2025-04-19 08:12:00 +0200

    up

--- linefollower/main.py
+++ linefollower/main.py
@@ -1,46 +1,10 @@
 import sys
 sys.path.append("/libs")
-from machine import Pin, ADC, PWM, SoftI2C
-from ssd1306 import SSD1306_I2C
-from time import sleep
-sensor_1 = ADC(Pin(0))
-sensor_2 = ADC(Pin(1))
+sys.path.append("/mode")
+import _thread
 
-i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
-oled_width = 128
-oled_height = 64
-oled = SSD1306_I2C(oled_width, oled_height, i2c)
+import auto
+import manual
 
-en_1 = PWM(Pin(21))
-en_1.freq(20000)
-en_1_in_1 = Pin(3, Pin.OUT)
-en_1_in_2 = Pin(2, Pin.OUT)
-en_1_in_1.value(1)
-en_1_in_2.value(0)
-
-en_2 = PWM(Pin(20))
-en_2.freq(20000)
-en_2_in_3 = Pin(5, Pin.OUT)
-en_2_in_4 = Pin(6, Pin.OUT)
-en_2_in_3.value(0)
-en_2_in_4.value(1)
-
-while True:
-    if (sensor_1.read_u16() < 50000):
-        en_1.duty_u16(0)
-    else:
-        en_1.duty_u16(65535)
-        
-    if (sensor_2.read_u16() < 50000):
-        en_2.duty_u16(0)
-    else:
-        en_2.duty_u16(65535)
-    
-    oled.fill(0)
-    oled.text("ADC 1: "+str(sensor_1.read_u16()), 0, 0)
-    oled.text("ADC 2: "+str(sensor_2.read_u16()), 0, 10)
-    oled.text("PWM 1: "+str(en_1.duty_u16()), 0, 30)
-    oled.text("PWM 2: "+str(en_2.duty_u16()), 0, 40)
-    oled.show()
-    
-    sleep(0.5)
+_thread.start_new_thread(auto.start, ())
+_thread.start_new_thread(manual.start, ())
---
+++ linefollower/mode/manual.py
@@ -0,0 +1,35 @@
+import network
+import socket
+from time import sleep
+
+def start():
+    print("MANUAL")
+
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid="LINEFOLLOWER", password="linefollower#557")
+
+    while not ap.active():
+        print("Starting...")
+        sleep(1)
+
+    print("Access Point active!")
+    print("IP:", ap.ifconfig()[0])
+
+    addr = socket.getaddrinfo("0.0.0.0", 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print("Server run on 0.0.0.0:80")
+
+    while True:
+        cl, addr = s.accept()
+        print("New conn", addr)
+
+        request = cl.recv(1024)
+        print("Res:", request)
+
+        cl.send("hi")
+        cl.close()
