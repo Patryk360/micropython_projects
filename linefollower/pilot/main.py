de5e53a7cdb6b76f851df91b65870561c92eb563
Patryk360 <nexigamingyt@gmail.com>
2025-06-17 23:18:13 +0200

    up

--- linefollower/pilot/main.py
+++ linefollower/pilot/main.py
@@ -1,25 +1,21 @@
 import sys
 sys.path.append("/libs")
+sys.path.append("/modes")
 
-import network
-import socket
-import ujson
 from time import sleep
 from machine import Pin, SoftI2C
-import config
-from ADS1115 import *
+import linefollower
+import boat
 from ssd1306 import SSD1306_I2C
+from ADS1115 import *
 
 i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
 adc = ADS1115(i2c=i2c)
 adc.setVoltageRange_mV(ADS1115_RANGE_4096)
-
-i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
 oled = SSD1306_I2C(128, 64, i2c)
 
-SERVER_IP = "192.168.4.1"
-SERVER_PORT = 80
-JOY_RANGE = 13200
+sw_right = Pin(3, Pin.IN, Pin.PULL_UP)
+sw_left = Pin(2, Pin.IN, Pin.PULL_UP)
 
 def read(channel):
     adc.setCompareChannels(channel)
@@ -29,61 +25,29 @@ def read(channel):
     res = adc.getRawResult()
     return res
 
-def connect_wifi(ssid, password):
-    try:
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        if not wlan.isconnected():
-            print("Connecting to WiFi...")
-            wlan.connect(ssid, password)
-            for _ in range(20):
-                if wlan.isconnected():
-                    break
-                sleep(0.5)
-        if wlan.isconnected():
-            print("Connected with IP:", wlan.ifconfig()[0])
-            return True
-        else:
-            print("Failed to connect")
-            return False
-    except OSError as e:
-            print(f"{e}")
+mode = 0
+modes = ["linefollower", "boat", "test1", "test2", "test3"]
 
-def send_mode(x, y):
-    try:
-        addr = socket.getaddrinfo(SERVER_IP, SERVER_PORT)[0][-1]
-        s = socket.socket()
-        s.connect(addr)
-        data = ujson.dumps({"x": x, "y": y})
-        s.send(data.encode())
-
-        response = s.recv(1024)
-        print("Response from server:", response.decode())
-        s.close()
-    except Exception as e:
-        print("Socket error:", e)
-
-def scale_to_percent(value, center):
-    delta = value - center
-    percent = int((delta / JOY_RANGE) * 100)
-    return max(min(percent, 100), -100) 
-
-def main():
-    centerX = read(ADS1115_COMP_0_GND)
-    centerY = read(ADS1115_COMP_1_GND)
-    if connect_wifi(config.ssid, config.password):
-        while True:
-            x = scale_to_percent(read(ADS1115_COMP_0_GND), centerX)
-            y = scale_to_percent(read(ADS1115_COMP_1_GND), centerY)
-            
-            oled.fill(0)
-            oled.text(f"X:{x}", 0, 0)
-            oled.text(f"Y:{y}", 0, 10)
-            oled.show()
-            
-            print(read(ADS1115_COMP_0_GND))
-            print(x)
-            print(y)
-            send_mode(x, y)
-
-main()
+while True:
+    if read(ADS1115_COMP_2_GND) < 15000:
+        if mode < 4:
+            mode += 1
+            print("down")
+    if read(ADS1115_COMP_2_GND) > 25000:
+        if mode > 0:
+            mode -= 1
+            print("up")
+    
+    oled.fill(0)
+    oled.text(f"{mode+1}. {modes[mode]}", 0, 0)
+    oled.show()
+    print(sw_right.value())
+    if sw_right.value() == 0:
+        if mode == 0:
+            linefollower.start()
+            sleep(2)
+        if mode == 1:
+            boat.start()
+            sleep(2)
+    print(sw_left.value())
+    sleep(0.15)
---
+++ linefollower/pilot/modes/linefollower.py
@@ -0,0 +1,98 @@
+import sys
+sys.path.append("/libs")
+
+import network
+import socket
+import ujson
+from time import sleep
+from machine import Pin, SoftI2C
+import config
+from ADS1115 import *
+from ssd1306 import SSD1306_I2C
+
+import gc
+
+i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
+adc = ADS1115(i2c=i2c)
+adc.setVoltageRange_mV(ADS1115_RANGE_4096)
+oled = SSD1306_I2C(128, 64, i2c)
+
+SERVER_IP = "192.168.4.1"
+SERVER_PORT = 80
+JOY_RANGE = 13200
+
+def read(channel):
+    adc.setCompareChannels(channel)
+    adc.startSingleMeasurement()
+    while adc.isBusy():
+        pass
+    res = adc.getRawResult()
+    return res
+
+def connect_wifi(ssid, password):
+    try:
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        if not wlan.isconnected():
+            print("Connecting to WiFi...")
+            oled.fill(0)
+            oled.text("Connecting...", 0, 0)
+            oled.show()
+            wlan.connect(ssid, password)
+            for _ in range(20):
+                if wlan.isconnected():
+                    break
+                sleep(0.5)
+        if wlan.isconnected():
+            print("Connected with IP:", wlan.ifconfig()[0])
+            return True
+        else:
+            print("Failed to connect")
+            oled.fill(0)
+            oled.text("Failed to connect", 0, 0)
+            oled.show()
+            return False
+    except OSError as e:
+            print(f"{e}")
+
+def send_mode(x, y):
+    try:
+        addr = socket.getaddrinfo(SERVER_IP, SERVER_PORT)[0][-1]
+        s = socket.socket()
+        s.connect(addr)
+        data = ujson.dumps({"x": x, "y": y})
+        s.send(data.encode())
+
+        response = s.recv(1024)
+        print("Response from server:", response.decode())
+        s.close()
+    except Exception as e:
+        print("Socket error:", e)
+
+def scale_to_percent(value, center):
+    delta = value - center
+    percent = int((delta / JOY_RANGE) * 100)
+    return max(min(percent, 100), -100) 
+
+def start():
+    centerX = read(ADS1115_COMP_0_GND)
+    centerY = read(ADS1115_COMP_1_GND)
+    if connect_wifi(config.ssid, config.password):
+        while True:
+            gc.collect()
+            
+            x = scale_to_percent(read(ADS1115_COMP_0_GND), centerX)
+            y = scale_to_percent(read(ADS1115_COMP_1_GND), centerY)
+            mem = gc.mem_free()
+            
+            oled.fill(0)
+            oled.text(f"X:{x}", 0, 0)
+            oled.text(f"Y:{y}", 0, 10)
+            oled.text(f"MEM:{mem/1024}", 0, 20)
+            oled.show()
+            
+            print(read(ADS1115_COMP_0_GND))
+            print(x)
+            print(y)
+            send_mode(x, y)
+            sleep(0.05)
